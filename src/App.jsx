032abc8d2585b2bcca3a499b1c30e24b61c964e8
Bruno Cechet <brunocechet@gmail.com>
2020-10-05 11:41:48 -0300

    App - Application entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+// Routing
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Material
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
+
+// Container Components
+import City from './containers/City';
+import Home from './containers/Home';
+
+// Utils
+import theme from './theme';
+
+// Styles
+import styles from './App.styles';
+
+const useStyles = makeStyles(styles);
+
 function App() {
-  return <div></div>;
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container className={classes.container}>
+        <Router>
+          <Switch>
+            <Route path="/city/:name">
+              <City />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Components
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// Utils
 import 'airbnb-browser-shims';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
