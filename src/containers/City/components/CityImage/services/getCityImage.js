d09b2bc2279a2f050f2dce332e451e5cd2956578
Bruno Cechet <brunocechet@gmail.com>
2020-10-05 22:41:06 -0300

    Fetching random imagem about city from Flickr

--- src/containers/City/components/CityImage/services/getCityImage.js
+++ src/containers/City/components/CityImage/services/getCityImage.js
@@ -1,25 +1,29 @@
+import getRandomInt from './utils/getRandomInt';
+
 const { REACT_APP_FLICK_API_URL, REACT_APP_FLICKR_API_KEY } = process.env;
 
 async function getCityImage(lat, lon) {
   const params = new URLSearchParams();
 
-  params.append('method', 'flickr.photos.search');
   params.append('api_key', REACT_APP_FLICKR_API_KEY);
-  params.append('sort', 'interestingness-desc');
   params.append('content_type', 1);
-  params.append('media', 'photos');
+  params.append('format', 'json');
+  params.append('is_getty', true);
   params.append('lat', lat);
   params.append('lon', lon);
-  params.append('is_getty', true);
-  params.append('per_page', 1);
-  params.append('format', 'json');
+  params.append('media', 'photos');
+  params.append('method', 'flickr.photos.search');
   params.append('nojsoncallback', 1);
+  params.append('per_page', 5);
+  params.append('sort', 'interestingness-desc');
 
   const {
     photos: { photo }
   } = await fetch(`${REACT_APP_FLICK_API_URL}?${params.toString()}`).then(response => response.json());
 
-  const cityImageData = photo?.[0] ?? {};
+  const cityPhotos = photo ?? [];
+
+  const cityImageData = cityPhotos[getRandomInt(cityPhotos.length)] ?? {};
 
   const { server, id, secret, title } = cityImageData;
 
