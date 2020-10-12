5fe3551bb718fee69a1cec7207b3df70c48b36e3
Bruno Cechet <brunocechet@gmail.com>
2020-10-11 22:21:57 -0300

    Creating the two rows behavior entry point

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+
 // Components
 import SearchForm from './components/SearchForm';
 import SearchHistory from './components/SearchHistory';
 
 function Home() {
   return (
-    <>
-      <SearchForm />
-      <SearchHistory />
-    </>
+    <Grid container alignItems="center">
+      <Grid item xs={12}>
+        <SearchForm />
+      </Grid>
+      <Grid item xs={12}>
+        <SearchHistory />
+      </Grid>
+    </Grid>
   );
 }
 
