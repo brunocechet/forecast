c486715ad0ad7d0cc4d1a898ed7fb5d2d097ba8d
Bruno Cechet <brunocechet@gmail.com>
2020-10-11 22:20:35 -0300

    Putting some styles to SearchHistory
    Also adding the remove city funcionality.

--- src/containers/Home/components/SearchHistory/SearchHistory.jsx
+++ src/containers/Home/components/SearchHistory/SearchHistory.jsx
@@ -1,11 +1,62 @@
 import React from 'react';
 
+import { Button, IconButton, makeStyles, Typography, Grid } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+
 import useSearchHistory from 'hooks/useSearchHistory';
 
-function SearchHistory() {
-  const [, { items }] = useSearchHistory();
+import { useHistory } from 'react-router-dom';
 
-  return items.map(item => <span key={item}>{item}</span>);
+// Styles
+import styles from './SearchHistory.styles';
+
+const useStyles = makeStyles(styles);
+
+function SearchHistory() {
+  const { items, remove } = useSearchHistory();
+
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleItemClick = cityName => {
+    cityName && history.push(`/city/${cityName}`);
+  };
+
+  const handleDeleteItem = cityName => {
+    remove(cityName);
+  };
+
+  const renderItems = () =>
+    items.map(item => (
+      <React.Fragment key={item}>
+        <Button
+          variant="outlined"
+          color="primary"
+          aria-label={`search for ${item}`}
+          title={`Search for ${item}`}
+          onClick={() => handleItemClick(item)}
+          className={classes.historyButton}
+        >
+          {item}
+        </Button>
+        <IconButton aria-label="delete" className={classes.deleteButton} onClick={() => handleDeleteItem(item)}>
+          <DeleteIcon fontSize="small" />
+        </IconButton>
+      </React.Fragment>
+    ));
+
+  return items.length ? (
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography align="center" variant="h4">
+          Search History
+        </Typography>
+      </Grid>
+      <Grid item xs={12} className={classes.buttonsContainer}>
+        {renderItems()}
+      </Grid>
+    </Grid>
+  ) : null;
 }
 
 export default SearchHistory;
---
+++ src/containers/Home/components/SearchHistory/SearchHistory.styles.js
@@ -0,0 +1,18 @@
+const styles = theme => ({
+  buttonsContainer: {
+    marginTop: theme.spacing(3),
+    textAlign: 'center'
+  },
+  historyButton: {
+    margin: theme.spacing(1),
+    width: theme.spacing(16),
+    height: theme.spacing(16)
+  },
+  deleteButton: {
+    position: 'absolute',
+    marginLeft: '-52px',
+    marginTop: '10px'
+  }
+});
+
+export default styles;
