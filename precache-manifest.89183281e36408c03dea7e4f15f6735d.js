d0815f30a6d97f112014bdb05ac12448867fbb41
Bruno Cechet <brunocechet@gmail.com>
2020-10-11 23:42:29 -0300

    Updates

---
+++ precache-manifest.89183281e36408c03dea7e4f15f6735d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b15b62e0de67e2edeb0da8d18b887e",
+    "url": "/forecast/index.html"
+  },
+  {
+    "revision": "86ab8efbefd3bfdead1b",
+    "url": "/forecast/static/js/2.f0415908.chunk.js"
+  },
+  {
+    "revision": "ca521ce1ed0d174fb12c5b2dcbfd3590",
+    "url": "/forecast/static/js/2.f0415908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfec5fce13467df0bf3b",
+    "url": "/forecast/static/js/main.9ac23885.chunk.js"
+  },
+  {
+    "revision": "253fb4b5af880c36251c",
+    "url": "/forecast/static/js/runtime-main.e8f0e08c.js"
+  }
+]);
