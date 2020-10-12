b26369e8cabde63142ef4dd09c07ce2bf80420ab
Bruno Cechet <brunocechet@gmail.com>
2020-10-11 23:23:32 -0300

    Mocks for mapbox-gl and localStorage

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,20 @@
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  GeolocateControl: jest.fn(),
+  Map: jest.fn(() => ({
+    addControl: jest.fn(),
+    on: jest.fn(),
+    remove: jest.fn()
+  })),
+  NavigationControl: jest.fn()
+}));
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn()
+};
+
+global.localStorage = localStorageMock;
