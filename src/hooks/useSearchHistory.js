020d7d637e4895f3e1c453664330e5f032fcc3c5
Bruno Cechet <brunocechet@gmail.com>
2020-10-11 22:20:05 -0300

    Adjusting hook to work with localStorage and state

--- src/hooks/useSearchHistory.js
+++ src/hooks/useSearchHistory.js
@@ -1,15 +1,20 @@
-import { useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 import { useLocalStorage } from 'react-use';
 
 const LOCALSTORAGE_KEY = 'forecast-history';
 
 function useSearchHistory() {
-  const [items, setItems] = useLocalStorage(LOCALSTORAGE_KEY, []);
+  const [localStorageItems, setLocalStorageItems] = useLocalStorage(LOCALSTORAGE_KEY, []);
+  const [stateItems, setStateItems] = useState(localStorageItems);
+
+  useEffect(() => {
+    setLocalStorageItems(stateItems);
+  }, [setLocalStorageItems, stateItems]);
 
   const handleAddNewItem = useCallback(
     newItem => {
-      setItems(oldItems => {
+      setStateItems(oldItems => {
         // eslint-disable-next-line prefer-const
         let newItems = oldItems.filter(item => item !== newItem);
         newItems.unshift(newItem);
@@ -18,10 +23,21 @@ function useSearchHistory() {
         return newItems;
       });
     },
-    [setItems]
+    [setStateItems]
   );
 
-  return [handleAddNewItem, { items }];
+  const handleRemoveItem = useCallback(
+    removeItem => {
+      setStateItems(oldItems => {
+        const newItems = oldItems.filter(item => item !== removeItem);
+
+        return newItems;
+      });
+    },
+    [setStateItems]
+  );
+
+  return { items: stateItems, add: handleAddNewItem, remove: handleRemoveItem };
 }
 
 export default useSearchHistory;
